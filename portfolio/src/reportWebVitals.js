a602ff99e0d21b2706c6d2921951d877c2fd6443
cudderson <codered1227@gmail.com>
2021-10-08 14:32:01 -0500

    remove CRA bloat

--- portfolio/src/reportWebVitals.js
+++ portfolio/src/reportWebVitals.js
@@ -1,3 +1,16 @@
+// keeping this file for now, but removing setupTests.js 
+// setupTests.js contained one line:
+//  - import '@testing-library/jest-dom';
+// learn more: https://github.com/testing-library/jest-dom
+//
+// I also deleted App.tests.js 
+// I also deleted a comment from index.js:
+// - If you want to start measuring performance in your app, pass a function
+//   to log results (for example: reportWebVitals(console.log))
+//   or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//   reportWebVitals();
+
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
