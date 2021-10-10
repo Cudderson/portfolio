011a10c04095f19f808a964743e4f6cbfe5665f4
cudderson <codered1227@gmail.com>
2021-10-09 22:42:11 -0500

    testing vscode git client

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,77 +4,39 @@ import { useState, useEffect } from 'react';
 
 // from now on, use arrow function (ES6) syntax to define components. syntax:
 // const Welcome = ({name}) => <h2>Hello, {name}</h2>;
-// a return statement must be provided if using curly braces
-
-
-// const Clock = () => {
-//   const [date, updateDate] = useState(new Date().toLocaleTimeString());
-
-//   return (
-//     <div>
-//       <h1>Hello world!</h1>
-//       <h2>It is {updateDate(date)}.</h2>
-//     </div>
-//   )
-// }
-
-// const tick = () => {
-//   ReactDOM.render(
-//     <Clock />,
-//     document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
-
-// *** below is an example of using Hooks (useState, useEffect) in stateful functional React components (ES6 arrow function) ***
-
-// function Example() {
-//   const [count, setCount] = useState(0);
-
-//   // Similar to componentDidMount and componentDidUpdate:
-//   useEffect(() => {
-//     // Update the document title using the browser API
-//     document.title = `You clicked ${count} times`;
-//   });
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>
-//         Click me
-//       </button>
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(
-//   <Example />,
-//   document.getElementById("root")
-// );
 
 // CHALLENGE:
-// Create a component that renders a button that changes colors on each click (works)
+// Create a component that renders a button that changes colors each second
 
-// const Button = () => {
-//   const [color, changeColor] = useState('red');
+const Button = () => {
+  const [color, changeColor] = useState('blue');
 
-//   const getNewColor = () => {
-//     let r = Math.floor(Math.random() * 255);
-//     let g = Math.floor(Math.random() * 255);
-//     let b = Math.floor(Math.random() * 255);
+  const getNewColor = () => {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
 
-//     let new_color = `rgb(${r}, ${g}, ${b})`;
+    let new_color = `rgb(${r}, ${g}, ${b})`;
 
-//     return new_color;
-//   }
+    return new_color;
+  }
+
+  const tick = () => {
+      changeColor(getNewColor);
+      clearInterval(interval);
+  }
+
+  var interval = setInterval(tick, 1000);
  
-//   return (
-//     <button style={{backgroundColor: color, fontSize: '30px'}} onClick={() => changeColor((getNewColor))}>Click Me To Change My Color!</button>
-//   )
-// }
+  return (
+    <button style={{backgroundColor: color, fontSize: '25px', margin: '4rem 0 0 35%', border: 'none', borderRadius: '4px', padding: '1rem 1.5rem'}}>
+        I'm a color-changing button!
+    </button>
+  )
+}
 
-// ReactDOM.render(
-//   <Button />,
-//   document.getElementById("root")
-// )
+
+ReactDOM.render(
+    <Button />,
+    document.getElementById('root')
+);
