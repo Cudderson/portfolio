fa63a2814e347229c2cd1517e0b4e98a20fc04ad
cudderson <codered1227@gmail.com>
2021-10-10 23:07:29 -0500

    test component file tree with git/vscode

---
+++ portfolio/src/components/ColorChangingButton.js
@@ -0,0 +1,32 @@
+// CHALLENGE:
+// Create a component that renders a button that changes colors each second
+import { useState } from "react";
+
+const ColorChangingButton = () => {
+    const [color, changeColor] = useState('blue');
+  
+    const getNewColor = () => {
+      let r = Math.floor(Math.random() * 255);
+      let g = Math.floor(Math.random() * 255);
+      let b = Math.floor(Math.random() * 255);
+  
+      let new_color = `rgb(${r}, ${g}, ${b})`;
+  
+      return new_color;
+    }
+  
+    const tick = () => {
+        changeColor(getNewColor);
+        clearInterval(interval);
+    }
+  
+    var interval = setInterval(tick, 1000);
+   
+    return (
+      <button style={{backgroundColor: color, fontSize: '40px', margin: '4rem 0 0 37%', border: 'none', borderRadius: '4px', padding: '1.25rem 2rem'}}>
+          I love to create!
+      </button>
+    )
+}
+
+export default ColorChangingButton;
