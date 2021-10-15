f357d65a356147ad6bd0516ad0acb899aa96921f
cudderson <codered1227@gmail.com>
2021-10-14 23:15:34 -0500

    create basic form/input with arrow function component and useState

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,17 +1,26 @@
-import ColorChangingButton from "./ColorChangingButton";
-import { useState } from "react";
+// import ColorChangingButton from "./ColorChangingButton";
+import { MyForm } from './MyForm';
+// import { useState } from "react";
 
 const App = () => {
-  const [visible, toggleVisibility] = useState(true);
-
-  const handleToggle = () => toggleVisibility(!visible);
-
-  return (
-    <div>
-      {visible ? <ColorChangingButton message={"I am visible!"} /> : null}
-      <button onClick={handleToggle}>show/hide</button>
-    </div>
-  )
+    return (
+        <div>
+            <MyForm />
+        </div>
+    )
 }
+
+// const App = () => {
+//   const [visible, toggleVisibility] = useState(true);
+
+//   const handleToggle = () => toggleVisibility(!visible);
+
+//   return (
+//     <div>
+//       {visible ? <ColorChangingButton message={"I am visible!"} /> : null}
+//       <button onClick={handleToggle}>show/hide</button>
+//     </div>
+//   )
+// }
 
 export default App
