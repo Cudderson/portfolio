552c7380c7853a7c8d2b4c2bcfbb6f8342fffc30
cudderson <codered1227@gmail.com>
2021-10-16 15:58:33 -0500

    create button to modify state and re-fetch new api data with custom hook

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,14 +1,29 @@
 import TodoList from "./TodoList";
 import { useFetch } from "./useFetch";
+import { useState } from "react";
 
 const App = () => {
 
-  let url = 'http://numbersapi.com/27/trivia';
+  const [number, setNumber] = useState(0);
 
-  useFetch(url);
+  const url = `http://numbersapi.com/${number}/trivia`;
+  const {data, loading} = useFetch(url);
+
+  // here, we could use useEffect to store the number in localStorage and reference it on every render to persist across a refresh
 
   return (
     <div>
+      <div>
+        {data ? data : 'loading...'}
+      </div>
+      <br />
+      <div>
+        <div>
+          {"Number: " + number}
+        </div>
+        <button onClick={() => setNumber(Math.floor(Math.random() * 1000))}>Get Random Number Fact</button>
+      </div>
+      <br />
       <TodoList />
     </div>
   )
--- portfolio/src/components/useFetch.js
+++ portfolio/src/components/useFetch.js
@@ -1,25 +1,23 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export const useFetch = (url) => {
+  const [fetchState, updateFetchState] = useState({data: null, loading: true});
+
+
   useEffect(() => {
+
+    updateFetchState(fetchState => ({ data: fetchState.data, loading: true }));
+    // not sure why yet, but the above is valid and the below raises a linting error in console
+    // maybe it's because it allows us to have access to the current state safely? (correct)
+    // updateFetchState({ data: fetchState.data, loading: true });
+
     // could use async/await if used another function within the anonymous
     fetch(url)
-      .then(x => x.text())
-      .then(y => {
-        console.log(y)
+      .then(res => res.text())
+      .then(res_text => {
+        updateFetchState({ data: res_text, loading: false })
       })
-  }, [url]);
+  }, [url, updateFetchState]);
+
+  return fetchState;
 }
-
-// equivalent to above
-// export const useFetch = (url) => {
-//   useEffect(() => {
-//     const getData = async () => {
-//       const res = await fetch(url);
-//       const text = await res.text();
-//       console.log(text);
-//     } 
-
-//     getData();
-//   }, [url]);
-// }
