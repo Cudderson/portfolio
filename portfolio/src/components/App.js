87c57eeef143b6c4c9b1e6d8a5f2c1f18d0a878e
cudderson <codered1227@gmail.com>
2021-10-20 21:15:51 -0500

    create header component, using css module design for styling

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,3 +1,13 @@
-const App = () => {}
+import Header from './Header';
+
+const App = () => {
+
+  // probably do this better in future (use/add class)
+  document.querySelector('body').style.margin = 0;
+
+  return (
+    <Header />
+  )
+}
 
 export default App;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      This is my header.
+    </header>
+  )
+}
+
+export default Header;
