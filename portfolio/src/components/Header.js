59e55d4d410a5b9daabd0dd5ce41e3e1e40eaaa4
cudderson <codered1227@gmail.com>
2021-10-21 21:35:14 -0500

    create dummy nav component, switch header from grid to flexbox layout

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,10 +1,12 @@
 import styles from './Header.module.css';
 import HeaderBrand from './HeaderBrand';
+import HeaderNav from './HeaderNav';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <HeaderBrand />
+      <HeaderNav />
     </header>
   )
 }
---
+++ portfolio/src/components/HeaderNav.js
@@ -0,0 +1,11 @@
+import styles from './HeaderNav.module.css';
+
+const HeaderNav = () => {
+  return (
+    <div className={styles['header-nav']}>
+      navigation
+    </div>
+  )
+}
+
+export default HeaderNav;
