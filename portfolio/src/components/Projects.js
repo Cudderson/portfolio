1917ce71cdff0dc29c96e9132e95b6ad74d4c000
cudderson <codered1227@gmail.com>
2021-10-22 22:42:24 -0500

    create grid component for projects, add dummy projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,9 +1,17 @@
 import styles from './Projects.module.css';
 
 const Projects = () => {
+  const getItems = () => {
+    const items = []
+    for (let i = 1; i < 5; i++) {
+      items.push(<div className={styles.item}>Project {i}</div>)
+    }
+    return items;
+  }
+
   return (
     <div className={styles.projects}>
-      my projects will go here!
+      {getItems()}
     </div>
   )
 }
