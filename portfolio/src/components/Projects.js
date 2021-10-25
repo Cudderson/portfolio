91d1ba3594807114738564491be1e7655f47bbca
cudderson <codered1227@gmail.com>
2021-10-24 22:21:47 -0500

    retrieve repository names from github api for projects component

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,18 +1,41 @@
+import { useState, useEffect } from 'react';
 import styles from './Projects.module.css';
 
 const Projects = () => {
-  const getItems = () => {
-    const items = []
-    for (let i = 1; i < 5; i++) {
-      items.push(<div className={styles.item}>Project {i}</div>)
-    }
-    return items;
+
+  const [api_data, updateApiData] = useState([]);
+
+  const getRepos = async () => {
+    const response = await fetch('https://api.github.com/users/cudderson/repos');
+    const json_obj = await response.json();
+    console.log(json_obj);
+
+    let repos = [];
+
+    json_obj.forEach(repo => {
+      repos.push(repo.name);
+    });
+
+    updateApiData(repos);
   }
+
+  useEffect(() => {
+    getRepos();
+  }, []);
 
   return (
     <div className={styles.projects}>
-      {getItems()}
+      <h2>Featured Projects</h2>
+      <h3>All projects built entirely by me!</h3>
+      {/* need keys for the below elements */}
+      {api_data.map(repo => (
+        <h6>{repo}</h6>
+      ))}
+      <div className={styles['projects-inner']}>
+        {/* projects will eventually be here */}
+      </div>
     </div>
+    
   )
 }
 
