59f527efd48219fddfdaee7e2981784e8e929218
cudderson <codered1227@gmail.com>
2021-11-10 23:00:52 -0600

    begin refactor of Project component design, modify some existing components

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,7 +1,7 @@
 import styles from './Project.module.css';
 
 // sub-components
-import ProjectFeatures from './ProjectFeatures.js';
+import ProjectFeatures from './ProjectFeatures.js'; // keep until <Project /> final
 import ImageSlider from './ImageSlider';
 
 const Project = (props) => {
@@ -23,14 +23,34 @@ const Project = (props) => {
           {props.description}
         </p>
 
-        {/*
-          here, I'm thinking of placing links to github/live
+        {/* this will be the link section */}
+        <div>
+          <h4 style={{marginBottom: '.25rem'}}>Links</h4>
+          <a href="##" style={{textDecoration: 0, fontSize: '18px'}}>Live Site</a>
+          <a href="##" style={{textDecoration: 0, fontSize: '18px', marginLeft: '20px'}}>Github Repo</a>
+        </div>
 
-          additionally, I will place the "Made With" section here
-        */}
+        {/* this will be the "Made With" section */}
+        <div>
+          <h4 style={{marginBottom: '.25rem'}}>Made With</h4>
 
+          {/* going to put a modified version of project-tech here for now until fa-icons */}
+          <div className={styles['project-tech']}>
+            <ul className={styles['project-tech-list']}>
+              {props.badges.map((badge) => (
+                <li className={styles['tech-box-test']} key={badge.toString()}>
+                  <img src={badge}></img>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+        </div>
+
+        {/* keep this component around until Project component final */}
         {/* <ProjectFeatures features={props.features} /> */}
       </div>
+      {/* keeping this original sub-component in-tact until Project component final */}
       {/* <div className={styles['project-tech']}>
         Technologies Used:
         <ul className={styles['project-tech-list']}>
