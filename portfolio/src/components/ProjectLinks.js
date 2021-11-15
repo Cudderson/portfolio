dea53e2307685c1c51987a1e26a74aa8390cbd5b
cudderson <codered1227@gmail.com>
2021-11-14 21:26:03 -0600

    link new fonts to test

--- portfolio/src/components/ProjectLinks.js
+++ portfolio/src/components/ProjectLinks.js
@@ -5,10 +5,10 @@ const ProjectLinks = (props) => {
     <div className={styles['project-links']}>
       <h4>Links</h4>
       {props.live_url ? 
-        <a href={props.live_url} className={styles['live-link']}>Live Site</a>
+        <a href={props.live_url} className={styles['live-link']}>View Live</a>
         : null
       }
-      <a href={props.repo_url} className={styles['repo-link']}>Github Repo</a>
+      <a href={props.repo_url} className={styles['repo-link']}>View Code</a>
     </div>
   )
 }
