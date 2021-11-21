e79d6448fd1c7b29293bfd33b69f43a9354ad6b2
cudderson <codered1227@gmail.com>
2021-11-21 16:29:18 -0600

    add github/linkedin profile links in ContactForm component

--- portfolio/src/components/ContactForm.js
+++ portfolio/src/components/ContactForm.js
@@ -5,11 +5,28 @@ const ContactForm = () => {
     // image images on either side of this container
     <div className={styles['contact-form-container']}>
       <div className={styles['contact-image-container']}>
+        {/* need to change */}
         <img src="https://avatars.githubusercontent.com/u/74884805?v=4"></img>
       </div>
+      <p>
+        Let's get in touch!
+      </p>
       <div className={styles['contact-form']}>
-        <p>Let's get in touch!</p>
-        <button><a href="mailto:codered1227@gmail.com" className={styles['email-link']}>Contact By Email</a></button>
+        <button>
+          <a href="https://github.com/Cudderson">
+            Github Profile
+          </a>
+        </button>
+        <button>
+          <a href="mailto:codered1227@gmail.com">
+            Contact By Email
+          </a>
+        </button>
+        <button>
+          <a href="https://www.linkedin.com/in/cody-weller-a5ba80222">
+            LinkedIn Profile
+          </a>
+        </button>
       </div>
     </div>
   )
