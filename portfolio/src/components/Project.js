bdff9b7c1e4b6bbe92e8bbf43879d854c998af08
cudderson <codered1227@gmail.com>
2021-11-22 22:21:24 -0600

    utilize useRef, useEffect and IntersectionObserver API to trigger css animation on Project component

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -3,14 +3,44 @@ import styles from './Project.module.css';
 // sub-components
 // import ProjectFeatures from './ProjectFeatures.js'; // keep until <Project /> final
 import ImageSlider from './ImageSlider.js';
-
-// tesing fa icons (this will be extracted to sub component)
 import ProjectTech from './ProjectTech.js';
-// import ProjectTechIcon from './ProjectTechIcon.js';
 
 import ProjectLinks from './ProjectLinks.js';
+import { useEffect, useRef } from 'react';
 
 const Project = (props) => {
+
+  // testing scroll animations with test useRef and dummy state
+  const test_ref = useRef(null);
+
+  // this should only appear on initial project renders (4 times)
+  console.log('rerendered!')
+
+  // add highlight class when element comes into view
+  // we are creating eventListeners on the initial render in useEffect()
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // proper way to add class with current method
+          entry.target.classList.add(styles.highlight);
+
+          // we don't need to rerender the component, as we're just adding a class
+          console.log("class added");
+        }
+        else {
+          // remove class when element leaves visibility (make sure you want this)
+          entry.target.classList.remove(styles.highlight);
+          console.log("class removed");
+        }
+      })
+    });
+  
+    // 'current' refers to the mounted <h2> element
+    if (test_ref.current) {
+      observer.observe(test_ref.current);
+    }
+  }, [test_ref.current]);  // depedency array could be empty, but populating for now
 
   return (
     // consider using element selectors in css rather than many classes
@@ -22,7 +52,7 @@ const Project = (props) => {
         <ImageSlider images={props.images} />
       </div>
       <div className={styles['project-text']}>
-        <h2 className={styles['project-title']}>
+        <h2 className={styles['project-title']} ref={test_ref}>
           {props.name}
         </h2>
         <p className={styles['project-description']}>
