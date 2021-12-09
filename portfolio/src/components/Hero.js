85eb7f3e5d22cc66c5d76e6c911d033673ac3d6a
cudderson <codered1227@gmail.com>
2021-12-08 22:06:10 -0600

    update copy in Hero component

--- portfolio/src/components/Hero.js
+++ portfolio/src/components/Hero.js
@@ -3,10 +3,11 @@ import styles from './Hero.module.css';
 const Hero = () => {
   return (
     <div className={styles['hero-container']}>
-      <h2>I'm a web developer from Wisconsin.</h2>
+      <h2>I'm a frontend developer</h2>
+      <h2>with a passion for the modern web.</h2>
+      {/* <h2>I'm a web developer from Wisconsin.</h2>
       <h2>I create efficient tech solutions using</h2>
-      <h2>passion, persistence, and a positive attitude.</h2>
-      {/* <div className={styles['floating-image']}></div> */}
+      <h2>passion, persistence, and a positive attitude.</h2> */}
     </div>
   )
 }
