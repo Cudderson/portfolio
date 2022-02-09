2ff65297e3b300aa7e6bdf7c30b51368b80bca5b
cudderson <codered1227@gmail.com>
2022-02-09 17:54:59 -0600

    begin establishing global color themes

--- portfolio/src/components/HeaderNav.js
+++ portfolio/src/components/HeaderNav.js
@@ -5,14 +5,12 @@ const HeaderNav = () => {
 
   return (
     <div className={styles['header-nav']}>
-      <ul>
         <a href="https://github.com/Cudderson" target="_blank" rel="noreferrer">
           <FontAwesomeIcon className={styles.icon} icon={['fab', 'github']} />
         </a>
         <a href="https://www.linkedin.com/in/cody-weller-a5ba80222/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon className={styles.icon} icon={['fab', 'linkedin']} />
         </a>
-      </ul>
     </div>
   )
 }
