2a7a82e5def8bcb916373a62c197e16dc97cdc19
cudderson <codered1227@gmail.com>
2022-02-13 13:25:43 -0600

    update about me copy, switch light theme gradient

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -18,11 +18,13 @@ const AboutMe = () => {
           last ~2 years of my life to improving my competency as a developer.
         </p>
         <p>
-          When I'm not on my computer, I enjoy reading, writing, and playing guitar.
+           With the desire to translate my passion into a professional career, I enrolled in and completed multiple 
+           web development and programming courses online, such as The Odin Project and Harvard's CS50x. 
           {/* In the short Wisconsin summertime, I love to play baseball, disc golf, and take long bike rides.  */}
         </p>
         <p>
-          In the short Wisconsin summertime, I love to play baseball, disc golf, and take long bike rides.
+          When I'm not on my computer, I enjoy reading, writing, and playing guitar.
+          In the summertime, I love playing baseball, disc golf, and taking long bike rides.
         </p>
       </div>
     </div>
