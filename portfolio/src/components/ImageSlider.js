43f09d87b25b6844ebd3c42a76ac6a18e4499875
cudderson <codered1227@gmail.com>
2022-02-17 19:34:13 -0600

    define animation to play on photo change in <ImageSlider/>

--- portfolio/src/components/ImageSlider.js
+++ portfolio/src/components/ImageSlider.js
@@ -1,12 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "./ImageSlider.module.css";
 
 const ImageSlider = (props) => {
-  // this state could be lifted to <Project />
   const [display_index, setDisplayIndex] = useState(0);
+  const sliderElement = useRef(null);
+
+  useEffect(() => {
+    // when the display index changes, re-run the animation
+    if (display_index !== 0) {
+      sliderElement.current.classList.add(styles['flash-animation']);
+
+
+      // TODO: remove class when animation ends, so that it will replay on next transition
+      // currently, it works, but breaks if you click a photo before the animation is over
+      sliderElement.current.addEventListener('animationstart', () => {
+        sliderElement.current.classList.add(styles['freeze']);
+      })
+
+      sliderElement.current.addEventListener('animationend', () => {
+        sliderElement.current.classList.remove(styles['flash-animation']);
+        sliderElement.current.classList.remove(styles['freeze']);
+      })
+    }
+  }, [display_index]);
 
   const calcDisplayIndex = (button_pressed) => {
+
     if (button_pressed === "prev") {
       setDisplayIndex(
         display_index === 0 ? props.images.length - 1 : display_index - 1
@@ -19,9 +39,9 @@ const ImageSlider = (props) => {
   };
 
   return (
-    <div className={styles["image-slider"]}>
+    <div className={styles["image-slider"]} ref={sliderElement}>
       <img src={props.images[display_index]} alt="" />
-      <button className={styles.prev} onClick={() => calcDisplayIndex("prev")}>
+      <button className={styles.prev} onClick={() => calcDisplayIndex("prev")} >
         <FontAwesomeIcon icon={["fas", "long-arrow-alt-left"]} />
       </button>
       <button className={styles.next} onClick={() => calcDisplayIndex("next")}>
